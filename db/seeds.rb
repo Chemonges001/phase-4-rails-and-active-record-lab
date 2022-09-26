1fb2d475b0979d7355fca290b8812d3d10a0f7cf
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-09-26 13:54:25 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name: 'Cheddar', last_name: 'Kiprop', grade: 5)
+Student.create!(first_name: 'Oscar', last_name: 'Kiprop', grade: 7)
